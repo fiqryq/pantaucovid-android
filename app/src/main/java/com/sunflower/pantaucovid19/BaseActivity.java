459124ad5c5b814a4fd0ce05135f0884759c2f2d
amirisback <faisalamircs@gmail.com>
2020-04-04 15:14:02 +0700

    add: Parent Inheritance Class

---
+++ app/src/main/java/com/sunflower/pantaucovid19/BaseActivity.java
@@ -0,0 +1,51 @@
+package com.sunflower.pantaucovid19;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * Pantau-Covid19
+ * Copyright (C) 04/04/2020.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.sunflower.pantaucovid19
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    protected AppCompatActivity mActivity;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+    }
+
+    protected void showToastShort(String message) {
+        Toast.makeText(mActivity, message, Toast.LENGTH_SHORT).show();
+    }
+
+    protected void showToastLong(String message) {
+        Toast.makeText(mActivity, message, Toast.LENGTH_LONG).show();
+    }
+
+    protected void setupFragment(Fragment fragment, int layout) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(layout, fragment)
+                .commit();
+    }
+
+}
