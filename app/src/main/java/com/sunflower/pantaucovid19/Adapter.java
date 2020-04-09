87c16d5c34c0745c82ce7a634ba78e713718fdb9
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-04-09 11:09:57 +0700

    Get Data Response to arraylist

--- app/src/main/java/com/sunflower/pantaucovid19/Adapter.java
+++ app/src/main/java/com/sunflower/pantaucovid19/Adapter.java
@@ -1,36 +1,60 @@
 package com.sunflower.pantaucovid19;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.sunflower.pantaucovid19.model.ModelData;
 
+import java.util.ArrayList;
+import java.util.zip.Inflater;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private Context mContext;
+    private ArrayList<ModelData> mData = new ArrayList<>();
+
+    public Adapter(Context mContext, ArrayList<ModelData> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view;
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.list_provinsi,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.mProvinsi.setText(mData.get(position).getProvinsi());
+        holder.mKasusPositif.setText(mData.get(position).getKasusPositif());
+        holder.mKasusNegatif.setText(mData.get(position).getKasusNegatif());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView mProvinsi;
+        private TextView mKasusPositif;
+        private TextView mKasusNegatif;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            mProvinsi = itemView.findViewById(R.id.provinsi);
+            mKasusPositif = itemView.findViewById(R.id.kasusPositif);
+            mKasusNegatif = itemView.findViewById(R.id.kasusNegatif);
         }
     }
 }
--- app/src/main/java/com/sunflower/pantaucovid19/fragment/HomeFragment.java
+++ app/src/main/java/com/sunflower/pantaucovid19/fragment/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sunflower.pantaucovid19.Adapter;
 import com.sunflower.pantaucovid19.R;
 import com.sunflower.pantaucovid19.base.BaseFragment;
 import com.sunflower.pantaucovid19.model.Model;
@@ -34,8 +37,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class HomeFragment extends Fragment {
 
-    private TextView tVresult;
     private ArrayList<ModelData> dataProvinsi = new ArrayList<>();
+    private Adapter adapter;
+    private RecyclerView provinsiRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,11 +49,13 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        tVresult = view.findViewById(R.id.textview_result);
+        provinsiRecyclerView = view.findViewById(R.id.rv_provinsi);
+        provinsiRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         dataResponse();
     }
 
     private void dataResponse(){
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.kawalcorona.com/indonesia/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -61,6 +67,9 @@ public class HomeFragment extends Fragment {
             @Override
             public void onResponse(Call<List<ModelData>> call, Response<List<ModelData>> response) {
                 dataProvinsi = new ArrayList<>(response.body());
+                adapter = new Adapter(getActivity(),dataProvinsi);
+                provinsiRecyclerView.setAdapter(adapter);
+                Log.d("dataprovinsi",dataProvinsi.toString());
             }
 
             @Override
