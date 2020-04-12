9f4827601ba1373d9dbebaffa1ea00e5c1f91a74
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-04-12 11:04:32 +0700

    Add Activity About : circle image , picasso.

--- app/src/main/java/com/sunflower/pantaucovid19/ui/activity/AboutActivity.java
+++ app/src/main/java/com/sunflower/pantaucovid19/ui/activity/AboutActivity.java
@@ -3,14 +3,27 @@ package com.sunflower.pantaucovid19.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 import com.sunflower.pantaucovid19.R;
 
 public class AboutActivity extends AppCompatActivity {
+    private ImageView mFiqry ,mAmir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        mFiqry = findViewById(R.id.imageViewFiqry);
+        mAmir = findViewById(R.id.imageViewAmir);
+        getProfileImage();
+    }
+
+    private void getProfileImage(){
+        String gambarFiqry = "http://pantaucovid.fiqrychoerudin.space/image/25787603.jpg";
+        String gambarAmir = "http://pantaucovid.fiqrychoerudin.space/image/24654871.jpg";
+        Picasso.get().load(gambarFiqry).into(mFiqry);
+        Picasso.get().load(gambarAmir).into(mAmir);
     }
 }
--- app/src/main/java/com/sunflower/pantaucovid19/ui/fragment/InfoFragment.java
+++ app/src/main/java/com/sunflower/pantaucovid19/ui/fragment/InfoFragment.java
@@ -1,7 +1,10 @@
 package com.sunflower.pantaucovid19.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sunflower.pantaucovid19.R;
+import com.sunflower.pantaucovid19.ui.activity.AboutActivity;
 
 
 /**
@@ -17,11 +21,27 @@ import com.sunflower.pantaucovid19.R;
  */
 public class InfoFragment extends Fragment {
 
+    private TextView mAbout;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mAbout = view.findViewById(R.id.tv_aboutapp);
+        mAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentAbout();
+            }
+        });
+    }
+
+    private void IntentAbout() {
+        Intent intent = new Intent(getActivity(), AboutActivity.class);
+        startActivity(intent);
     }
 }
