4e6d4a614c42b68dfa1d58efc210226837d0de7b
Davin Alfarizky Putra Basudewa <dbasudewa@gmail.com>
2020-04-15 20:35:33 +0700

    Add location helper & fix InfoContributor -Adding global Permission Checker -Adding LocationManager from best provider (MobileNetwork or GPS)

--- app/src/main/java/com/sunflower/pantaucovid19/MainActivity.java
+++ app/src/main/java/com/sunflower/pantaucovid19/MainActivity.java
@@ -1,21 +1,29 @@
 package com.sunflower.pantaucovid19;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 import com.sunflower.pantaucovid19.base.BaseActivity;
 import com.sunflower.pantaucovid19.ui.fragment.BeritaFragment;
 import com.sunflower.pantaucovid19.ui.fragment.HomeFragment;
 import com.sunflower.pantaucovid19.ui.fragment.InfoFragment;
+import com.sunflower.pantaucovid19.utils.LocationTrack;
 
 public class MainActivity extends BaseActivity {
-
+    private LocationTrack locate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ChipNavigationBar bottomnav = findViewById(R.id.navbar);
         setupBottomNav(bottomnav);
+        locate = new LocationTrack(this);
+        if (locate.canGetLocation()){
+            Toast.makeText(this,"Lang: "+locate.getLatitude()+ " Long "+ locate.getLongitude(),Toast.LENGTH_LONG).show();
+        }else {
+            locate.showSettingsAlert();
+        }
     }
 
     private void setupBottomNav(ChipNavigationBar chipNavigationBar) {
@@ -36,4 +44,9 @@ public class MainActivity extends BaseActivity {
         chipNavigationBar.setItemSelected(R.id.home, true);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        locate.stopListener();
+    }
 }
