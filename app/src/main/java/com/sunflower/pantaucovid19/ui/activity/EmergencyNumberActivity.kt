81ac67385e7eb48ffeaa2605e8539a167119c2e5
Belmiro <tengku13elmiro@gmail.com>
2020-04-15 16:45:50 +0700

    - Add emergencyCallActivity

---
+++ app/src/main/java/com/sunflower/pantaucovid19/ui/activity/EmergencyNumberActivity.kt
@@ -0,0 +1,43 @@
+package com.sunflower.pantaucovid19.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.sunflower.pantaucovid19.R
+import kotlinx.android.synthetic.main.activity_emergency_number.*
+import java.util.jar.Manifest
+
+class EmergencyNumberActivity : AppCompatActivity() {
+
+    val numberCall ="119";
+    val REQUEST_PHONE_CALL = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency_number)
+        cd_call.setOnClickListener {
+            if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
+            }else{
+                startCall();
+            }
+
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startCall() {
+        val callIntent = Intent(Intent.ACTION_CALL)
+
+        callIntent.data = Uri.parse("tel:" +numberCall)
+
+        startActivity(callIntent)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_PHONE_CALL)startCall()
+    }
+}
